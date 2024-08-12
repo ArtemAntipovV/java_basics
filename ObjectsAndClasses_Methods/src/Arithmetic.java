fa5c2d3a8038e8b45a14ac61ae0aff7947780222
System <it@skillbox.ru>
2024-08-12 07:50:20 +0300

    Add lesson

---
+++ ObjectsAndClasses_Methods/src/Arithmetic.java
@@ -0,0 +1,26 @@
+public class Arithmetic {
+
+     private int firstNumber;
+     private int secondNumber;
+     int sum;
+     int multiNumber;
+
+
+    public Arithmetic( int firstNumber, int secondNumber) {
+            this.firstNumber = firstNumber;
+            this.secondNumber = secondNumber;
+        }
+    public static int sum(int firstNumber,int SecondNumber){
+        return firstNumber + SecondNumber;
+    }
+
+    public  static int multiNumber (int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+
+
+
+
+
+ }
