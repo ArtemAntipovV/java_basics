e66a418aefe6759882d03e6da5a1fbd1b05f51f0
Home <artem.antipov.2012@mail.ru>
2024-08-14 16:22:43 +0500

    Add lesson

--- ObjectsAndClasses_Methods/src/Main.java
+++ ObjectsAndClasses_Methods/src/Main.java
@@ -1,27 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Arithmetic arithmetic = new Arithmetic(5,6);
-        System.out.println("Сумма чисел: " + arithmetic.sum());
+//        Arithmetic arithmetic = new Arithmetic(5,6);
+//        System.out.println("Сумма чисел: " + arithmetic.sum());
+//
+//        Arithmetic arithmetic1 = new Arithmetic(5,6);
+//        System.out.println("Произведение чисел: " + arithmetic1.multiNumber());
+//
+//        Arithmetic arithmetic2 = new Arithmetic(5,6);
+//        System.out.println("Наибольшее число: " + arithmetic2.maximalNumber());
+//
+//        Arithmetic arithmetic3 = new Arithmetic(5,6);
+//        System.out.println("Наименьшее число: " + arithmetic3.minimalNumber());
+//
 
-        Arithmetic arithmetic1 = new Arithmetic(5,6);
-        System.out.println("Произведение чисел: " + arithmetic1.multiNumber());
 
-        Arithmetic arithmetic2 = new Arithmetic(5,6);
-        System.out.println("Наибольшее число: " + arithmetic2.max());
+
+
+
+
+
+       Basket basket = new Basket();
+       basket.add("Milk", 40);
+       basket.print("Milk");
+   }
+
 
     }
-}
-
-
-
-
-
-
-//        Basket basket = new Basket();
-//        basket.add("Milk", 40);
-//        basket.print("Milk");
-//    }
-
-
