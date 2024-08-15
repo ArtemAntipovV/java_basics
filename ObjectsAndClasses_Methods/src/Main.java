a48be565c50ba8694597cfbaf0585e915e7331c2
Home <artem.antipov.2012@mail.ru>
2024-08-15 12:37:30 +0500

    Add lesson

--- ObjectsAndClasses_Methods/src/Main.java
+++ ObjectsAndClasses_Methods/src/Main.java
@@ -2,17 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        Arithmetic arithmetic = new Arithmetic(5,6);
-//        System.out.println("Сумма чисел: " + arithmetic.sum());
-//
-//        Arithmetic arithmetic1 = new Arithmetic(5,6);
-//        System.out.println("Произведение чисел: " + arithmetic1.multiNumber());
-//
-//        Arithmetic arithmetic2 = new Arithmetic(5,6);
-//        System.out.println("Наибольшее число: " + arithmetic2.maximalNumber());
-//
-//        Arithmetic arithmetic3 = new Arithmetic(5,6);
-//        System.out.println("Наибольшее число: " + arithmetic3.minimalNumber());
+        Arithmetic arithmetic = new Arithmetic(5,6);
+        System.out.println("Сумма чисел: " + arithmetic.sum());
+
+        Arithmetic arithmetic1 = new Arithmetic(5,6);
+        System.out.println("Произведение чисел: " + arithmetic1.multiNumber());
+
+        Arithmetic arithmetic2 = new Arithmetic(5,6);
+        System.out.println("Наибольшее число: " + arithmetic2.maximalNumber());
+
+        Arithmetic arithmetic3 = new Arithmetic(5,6);
+        System.out.println("Наибольшее число: " + arithmetic3.minimalNumber());
 
 
 
