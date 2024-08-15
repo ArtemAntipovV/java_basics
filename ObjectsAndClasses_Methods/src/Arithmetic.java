c8e5964f6854f1e654f3e51e14e0a5289ec99048
Home <artem.antipov.2012@mail.ru>
2024-08-15 12:32:11 +0500

    Add lesson

--- ObjectsAndClasses_Methods/src/Arithmetic.java
+++ ObjectsAndClasses_Methods/src/Arithmetic.java
@@ -1,39 +1,39 @@
-//
-//public class Arithmetic {
-//
-//    int a;
-//    int b;
-//
-//
-//    public Arithmetic(int a, int b) {
-//        this.a = a;
-//        this.b = b;
-//    }
-//
-//    public int sum() {
-//        return a + b;
-//    }
-//
-//    public int multiNumber() {
-//        return a * b;
-//    }
-//
-//    public int maximalNumber() {
-//        if (a > b) {
-//            return a;
-//        }
-//        return b;
-//    }
-//    public int minimalNumber() {
-//        if (a > b) {
-//            return b;
-//        }
-//        return a;
-//    }
-//
-//
-//}
-//
+
+public class Arithmetic {
+
+    int a;
+    int b;
+
+
+    public Arithmetic(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int sum() {
+        return a + b;
+    }
+
+    public int multiNumber() {
+        return a * b;
+    }
+
+    public int maximalNumber() {
+        if (a > b) {
+            return a;
+        }
+        return b;
+    }
+    public int minimalNumber() {
+        if (a > b) {
+            return b;
+        }
+        return a;
+    }
+
+
+}
+
 
 
 
--- ObjectsAndClasses_Methods/src/Main.java
+++ ObjectsAndClasses_Methods/src/Main.java
@@ -9,18 +9,18 @@ public class Main {
 
     }
 }
-//        Arithmetic arithmetic = new Arithmetic(5,6);
-//        System.out.println("Сумма чисел: " + arithmetic.sum());
-//
-//        Arithmetic arithmetic1 = new Arithmetic(5,6);
-//        System.out.println("Произведение чисел: " + arithmetic1.multiNumber());
-//
-//        Arithmetic arithmetic2 = new Arithmetic(5,6);
-//        System.out.println("Наибольшее число: " + arithmetic2.maximalNumber());
-//
-//        Arithmetic arithmetic3 = new Arithmetic(5,6);
-//        System.out.println("Наименьшее число: " + arithmetic3.minimalNumber());
-//
+        Arithmetic arithmetic = new Arithmetic(5,6);
+        System.out.println("Сумма чисел: " + arithmetic.sum());
+
+        Arithmetic arithmetic1 = new Arithmetic(5,6);
+        System.out.println("Произведение чисел: " + arithmetic1.multiNumber());
+
+        Arithmetic arithmetic2 = new Arithmetic(5,6);
+        System.out.println("Наибольшее число: " + arithmetic2.maximalNumber());
+
+        Arithmetic arithmetic3 = new Arithmetic(5,6);
+        System.out.println("Наименьшее число: " + arithmetic3.minimalNumber());
+
 
 
 
