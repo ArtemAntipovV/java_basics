a04ef01dbc220e27069633763b49303aa1478894
Home <artem.antipov.2012@mail.ru>
2024-08-18 10:54:21 +0500

    homework

--- Encapsulation/src/Elevator.java
+++ Encapsulation/src/Elevator.java
@@ -14,18 +14,55 @@ public class Elevator {
     }
 
     public void moveDown() {
-        currentFloor = currentFloor > minFloor ? currentFloor - 1 : currentFloor; // уменьшающий значение переменной currentFloor на единицу
+        currentFloor = currentFloor > minFloor ? currentFloor - 1 : currentFloor;
+         // уменьшающий значение переменной currentFloor на единицу
     }
 
     public void moveUp() {
         currentFloor = currentFloor < maxFloor ? currentFloor + 1 : currentFloor; // перемещающий лифт на один этаж вверх.
-
     }
 
     public void move(int floor) {
+        if (currentFloor < floor) {
+            while (true) {
+                moveUp();
+                System.out.println("Этаж № " + currentFloor);
+                if (currentFloor == floor || currentFloor == maxFloor)
+                    break;
+            }
+        } else if (currentFloor > floor) {
+            while (true) {
+                moveDown();
+                System.out.println("Этаж №" + currentFloor);
+                if (currentFloor == minFloor) {
+                    break;
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+//        if (currentFloor == maxFloor) {
+//           return;
+//        }
+//        floor = floor + 1;
+//        System.out.println("Лифт перемещается на этаж " + floor);
+//        if (currentFloor == minFloor) {
+//            return;
+//        }
+//        floor = floor - 1;
+//        System.out.println("Лифт перемещается на этаж " + floor);
+    }
+
 
     }
 
 
-}
+
 
