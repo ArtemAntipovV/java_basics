a26f8074eb774d6880c25cb1f906a5c3869b3430
Home <artem.antipov.2012@mail.ru>
2024-10-08 15:49:32 +0500

    homework

--- StreamAPI/Airport/src/main/java/airport/Main.java
+++ StreamAPI/Airport/src/main/java/airport/Main.java
@@ -13,23 +13,19 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static long findCountAircraftWithModelAirbus(Airport airport, String model) {
-        //TODO Метод должен вернуть количество самолетов указанной модели.
-        // подходят те самолеты, у которых name начинается со строки model
+
         return airport.getAllAircrafts().stream()
                 .filter(a -> a.getModel().startsWith(model))
                 .count();
     }
 
     public static Map<String, Integer> findMapCountParkedAircraftByTerminalName(Airport airport) {
-        //TODO Метод должен вернуть словарь с количеством припаркованных самолетов в каждом терминале.
         return airport.getTerminals().stream()
                 .collect(Collectors.toMap(Terminal::getName, t -> t.getParkedAircrafts().size()));
 
     }
 
     public static List<Flight> findFlightsLeavingInTheNextHours(Airport airport, int hours) {
-        //TODO Метод должен вернуть список отправляющихся рейсов в ближайшее количество часов.
-
         Instant departureNow = Instant.now();
         Instant datePlusHours = departureNow.plus(hours, ChronoUnit.HOURS);
 
@@ -42,7 +38,6 @@ public class Main {
     }
 
     public static Optional<Flight> findFirstFlightArriveToTerminal(Airport airport, String terminalName) {
-        //TODO Найти ближайший прилет в указанный терминал.
                 Instant arriveNow = Instant.now();
 
         return airport.getTerminals()
