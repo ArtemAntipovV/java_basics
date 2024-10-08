5fea81fbdbfa3d2aaff0b38d09985be23b21195d
Home <artem.antipov.2012@mail.ru>
2024-10-08 15:35:02 +0500

    homework

--- StreamAPI/Airport/src/main/java/airport/Main.java
+++ StreamAPI/Airport/src/main/java/airport/Main.java
@@ -2,8 +2,13 @@ package airport;
 
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
+import com.skillbox.airport.Terminal;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -13,19 +18,28 @@ public class Main {
         // подходят те самолеты, у которых name начинается со строки model
         return airport.getAllAircrafts().stream()
                 .filter(a -> a.getModel().startsWith(model))
-        .count();
+                .count();
     }
 
     public static Map<String, Integer> findMapCountParkedAircraftByTerminalName(Airport airport) {
-
-
         //TODO Метод должен вернуть словарь с количеством припаркованных самолетов в каждом терминале.
-        return Collections.emptyMap();
+        return airport.getTerminals().stream()
+                .collect(Collectors.toMap(Terminal::getName, t -> t.getParkedAircrafts().size()));
+
     }
 
     public static List<Flight> findFlightsLeavingInTheNextHours(Airport airport, int hours) {
         //TODO Метод должен вернуть список отправляющихся рейсов в ближайшее количество часов.
-        return Collections.emptyList();
+
+        Instant dateNow = Instant.now();
+        Instant datePlusHours = dateNow.plus(hours, ChronoUnit.HOURS);
+
+        return airport.getTerminals().stream()
+                .flatMap(a -> a.getFlights().stream())
+                        .filter(x -> x.getType() == Flight.Type.DEPARTURE)
+                        .filter(y -> y.getDate().isBefore(datePlusHours)
+                        && y.getDate().isAfter(dateNow))
+                        .collect(Collectors.toList());
     }
 
     public static Optional<Flight> findFirstFlightArriveToTerminal(Airport airport, String terminalName) {
