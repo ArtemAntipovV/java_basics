81edcf53c2f552a419bdef350a6c0b5651fcc791
Home <artem.antipov.2012@mail.ru>
2024-10-15 15:54:38 +0500

    homework

--- FilesAndNetwork/HomeWorkJson/src/main/java/org/example/App.java
+++ FilesAndNetwork/HomeWorkJson/src/main/java/org/example/App.java
@@ -1,12 +1,16 @@
 package org.example;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        ParcingWeb htmlCode = new ParcingWeb();
-        htmlCode.getHtml("https://skillbox-java.github.io/");
-        System.out.println(htmlCode);
+        String html = ParcingWeb.getHtml("https://skillbox-java.github.io/");
+        System.out.println(html);
     }
 
 
--- FilesAndNetwork/HomeWorkJson/src/main/java/org/example/ParcingWeb.java
+++ FilesAndNetwork/HomeWorkJson/src/main/java/org/example/ParcingWeb.java
@@ -4,17 +4,19 @@ package org.example;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-import java.io.IOException;
-
 public class ParcingWeb {
 
-    public  String getHtml(String url) {
+
+
+    public static   String getHtml(String url) {
             try {
                 Document doc = Jsoup.connect(url).get();
                 return doc.html();
             } catch (Exception e) {
                e.printStackTrace();
-               return null;
+                return null;
             }
     }
+
+
 }
