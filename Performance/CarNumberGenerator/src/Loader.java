02dc66cc4fc0c3dac98caaa5f6b95d3106041426
Home <artem.antipov.2012@mail.ru>
2024-12-05 12:17:42 +0500

    homework1

--- Performance/CarNumberGenerator/src/Loader.java
+++ Performance/CarNumberGenerator/src/Loader.java
@@ -1,41 +1,64 @@
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Loader {
+
+    private static final int NUM_THREADS = 4;
+    private static final int REGIONS_PER_THREAD = 99 / NUM_THREADS;
+    private static final char[] LETTERS = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
 
     public static void main(String[] args) throws Exception {
         long start = System.currentTimeMillis();
 
-        FileOutputStream writer = new FileOutputStream("res/numbers.txt");
+        ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS);
 
-        char letters[] = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
-        for (int number = 1; number < 1000; number++) {
-            int regionCode = 199;
-            for (char firstLetter : letters) {
-                for (char secondLetter : letters) {
-                    for (char thirdLetter : letters) {
-                        String carNumber = firstLetter + padNumber(number, 3) +
-                            secondLetter + thirdLetter + padNumber(regionCode, 2);
-                        writer.write(carNumber.getBytes());
-                        writer.write('\n');
-                    }
-                }
-            }
+        for (int i = 0; i < NUM_THREADS; i++) {
+           int startRegion = i * REGIONS_PER_THREAD + 1;
+           int endRegion = Math.min(startRegion + REGIONS_PER_THREAD, 100);
+            Runnable task = () -> generateNumbers(startRegion, endRegion);
+            executor.submit(task);
         }
 
-        writer.flush();
-        writer.close();
+        executor.shutdown();
+        executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
 
         System.out.println((System.currentTimeMillis() - start) + " ms");
     }
 
-    private static String padNumber(int number, int numberLength) {
-        String numberStr = Integer.toString(number);
-        int padSize = numberLength - numberStr.length();
-
-        for (int i = 0; i < padSize; i++) {
-            numberStr = '0' + numberStr;
+        private static void generateNumbers (int startRegion, int endRegion) {
+            try (PrintWriter writer = new PrintWriter(new FileOutputStream("res/numbers_" + startRegion + "-" + endRegion + ".txt"))) {
+                for (int regionCode = startRegion; regionCode < endRegion; regionCode++) {
+                    for (int number = 1; number < 1000; number++) {
+                        for (char firstLetter : LETTERS) {
+                            for (char secondLetter : LETTERS) {
+                                for (char thirdLetter : LETTERS) {
+                                    writer.print(firstLetter);
+                                    writer.print(padNumber(number, 3));
+                                    writer.print(secondLetter);
+                                    writer.print(thirdLetter);
+                                    writer.print(padNumber(regionCode, 2));
+                                    writer.println();
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
-        return numberStr;
+        private static String padNumber ( int number, int numberLength) {
+            StringBuilder numberStr = new StringBuilder(Integer.toString(number));
+
+            while (numberStr.length() < numberLength) {
+                numberStr.insert(0, '0');
+            }
+            return numberStr.toString();
+        }
     }
-}
+
--- Performance/VoteAnalyzer/Vote/src/main/java/Loader.java
+++ Performance/VoteAnalyzer/Vote/src/main/java/Loader.java
@@ -21,22 +21,30 @@ public class Loader {
     public static void main(String[] args) throws Exception {
         String fileName = "res/data-1M.xml";
 
+//        SAXParserFactory factory = SAXParserFactory.newInstance();
+//        SAXParser parser = factory.newSAXParser();
+//        XMLHandler handler = new XMLHandler();
+//        parser.parse(new File(fileName), handler);
+//        handler.printDuplicatedVoters();
+
+        long start = System.currentTimeMillis();
         parseFile(fileName);
-
+        System.out.println("Parsing duration: " + (System.currentTimeMillis() - start) + " ms");
+        DBConnection.printVoterCounts();
         //Printing results
-        System.out.println("Voting station work times: ");
-        for (Integer votingStation : voteStationWorkTimes.keySet()) {
-            WorkTime workTime = voteStationWorkTimes.get(votingStation);
-            System.out.println("\t" + votingStation + " - " + workTime);
-        }
-
-        System.out.println("Duplicated voters: ");
-        for (Voter voter : voterCounts.keySet()) {
-            Integer count = voterCounts.get(voter);
-            if (count > 1) {
-                System.out.println("\t" + voter + " - " + count);
-            }
-        }
+//        System.out.println("Voting station work times: ");
+//        for (Integer votingStation : voteStationWorkTimes.keySet()) {
+//            WorkTime workTime = voteStationWorkTimes.get(votingStation);
+//            System.out.println("\t" + votingStation + " - " + workTime);
+//        }
+//
+//        System.out.println("Duplicated voters: ");
+//        for (Voter voter : voterCounts.keySet()) {
+//            Integer count = voterCounts.get(voter);
+//            if (count > 1) {
+//                System.out.println("\t" + voter + " - " + count);
+//            }
+//        }
     }
 
     private static void parseFile(String fileName) throws Exception {
@@ -56,13 +64,16 @@ public class Loader {
             NamedNodeMap attributes = node.getAttributes();
 
             String name = attributes.getNamedItem("name").getNodeValue();
-            Date birthDay = birthDayFormat
-                .parse(attributes.getNamedItem("birthDay").getNodeValue());
+           String birthDay = attributes.getNamedItem("birthDay").getNodeValue();
+//            Date birthDay = birthDayFormat
+//                .parse(attributes.getNamedItem("birthDay").getNodeValue());
 
-            Voter voter = new Voter(name, birthDay);
-            Integer count = voterCounts.get(voter);
-            voterCounts.put(voter, count == null ? 1 : count + 1);
+            DBConnection.countVoter(name, birthDay);
+//            Voter voter = new Voter(name, birthDay);
+//            Integer count = voterCounts.get(voter);
+//            voterCounts.put(voter, count == null ? 1 : count + 1);
         }
+        DBConnection.executeMultiInsert();
     }
 
     private static void fixWorkTimes(Document doc) throws Exception {
